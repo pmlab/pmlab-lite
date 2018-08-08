31e0da431f0fe04ad70a29dc42609e074ba2dfe1
sim-o-n <remysimo@hu-berlin.de>
2018-08-08 11:54:00 +0200

    add vizualization for simple graphs

--- pmlab_lite/helper/viz/dot.py
+++ pmlab_lite/helper/viz/dot.py
@@ -1,9 +1,11 @@
 from graphviz import Digraph
 
+from pmlab_lite.graph import Graph
 from pmlab_lite.pn import AbstractPetriNet
 
 
-def draw(input_net:AbstractPetriNet, filename="petri_net", format="pdf"):
+def draw_petri_net(input_net:AbstractPetriNet, filename="petri_net",
+				 format="pdf"):
 	"""
 	This function transforms the given Petri net structure
 	into DOT notation and returns it as a digraph object. Call
@@ -41,4 +43,24 @@ def draw(input_net:AbstractPetriNet, filename="petri_net", format="pdf"):
 		dot.edge(str(e[0]), str(e[1]))
 
 	# dot.render()
+	return dot
+
+def draw_graph(graph: Graph, filename="graph", format="pdf"):
+	dot = Digraph(name=filename, format=format)
+
+	dot.attr(rankdir='LR', fontsize="10", nodesep="0.35",
+			 ranksep="0.25 equally")
+
+	# draw nodes
+	dot.attr('node', penwidth="1", fontsize="10",
+			 fontname="Helvetica")
+
+	for node in graph.graph:
+		dot.node(str(node))
+
+	# draw edges
+	for node in graph.graph:
+		for target in graph.graph[node]:
+			dot.edge(str(node), str(target))
+
 	return dot
