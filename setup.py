74881edc1714f1db13f2ec368c5e8a19dd949f04
sim-o-n <remysimo@hu-berlin.de>
2018-09-04 11:50:59 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+	name='pmlab_lite',
+    version='0.1',
+    description='Process Mining scripting environment',
+	classifiers=[
+			'Development Status :: 2 - Pre-Alpha',
+			'Programming Language :: Python :: 3'
+		  ],
+    url='https://github.com/pmlab/pmlab-lite',
+    author=['Josep Carmona',
+			'Matthias Weidlich',
+			'Andrea Burattin',
+			'jmunozgama',
+			'Simon Remy'],
+    author_email='',
+    license='GPL-3.0',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
