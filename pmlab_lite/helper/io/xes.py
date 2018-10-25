6b8414cf61add49068843bac2c3cc9564f26feb5
Andrea Burattin <andrea.burattin@gmail.com>
2018-10-25 11:17:59 +0200

    Improved documentation and attributes parsing

--- pmlab_lite/helper/io/xes.py
+++ pmlab_lite/helper/io/xes.py
@@ -1,3 +1,4 @@
+from dateutil.parser import parse
 import gzip
 import xml.etree.ElementTree as xmltree
 from pmlab_lite.log import *
@@ -7,7 +8,7 @@ def import_from_xes(file):
     """Load a log from the XES format.
 
     [filename] can be a file or a filename."""
-    if isinstance(file, str):  # a filename
+    if isinstance(file, str):
         filename = file
         if filename.endswith('.gz'):
             file = gzip.open(filename, 'rb')
@@ -16,20 +17,38 @@ def import_from_xes(file):
     else:
         filename = file.name
 
+    # construct the new event log  object
+    imported_log = EventLog()
+
+    # identify all traces in the log (assuming just one process per log)
     ns = {'xes': 'http://www.xes-standard.org/'}
     tree = xmltree.parse(file)
     root = tree.getroot()
     traces = root.findall('xes:trace', ns)
-    imported_log = EventLog()
+
+    # parse all traces in the log
     for t in traces:
+        # extract the case id
         case_id = t.find('xes:string[@key="concept:name"]', ns).attrib['value']
+        # parse all events in the trace
         for e in t.findall('xes:event', ns):
+            # extract the activity name and construct the new event
             activity_name = e.find('xes:string[@key="concept:name"]', ns).attrib['value']
-            timestamp = e.find('xes:date[@key="time:timestamp"]', ns).attrib['value']
             event = Event(activity_name, case_id)
-            for a in e:
+            # parse all string attributes
+            for a in e.findall('xes:string', ns):
+                # we need to exclude the name of the event
                 if a.attrib['key'] != "concept:name":
                     event[a.attrib['key']] = a.attrib['value']
+            # parse all integer attributes
+            for a in e.findall('xes:int', ns):
+                event[a.attrib['key']] = int(a.attrib['value'])
+            # parse all date attributes
+            for a in e.findall('xes:date', ns):
+                event[a.attrib['key']] = parse(a.attrib['value'])
+            # parse all boolean attributes
+            for a in e.findall('xes:boolean', ns):
+                event[a.attrib['key']] = a.attrib['value'] == 'true'
             imported_log.add_event(event)
     return imported_log
 
@@ -38,4 +57,4 @@ def print_log(l):
     for t in l.get_traces():
         print("TRACE:")
         for e in t:
-            print("> ", e.get_activity_name(), e.get_case_id(), e.get_attributes())
+            print("> ", e.get_activity_name(), e.get_case_id(), e.values())
