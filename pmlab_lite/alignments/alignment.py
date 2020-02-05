d1df8ea5da313dc18d249b9a5b62651ab7daa98b
dittmannglenn <57635813+dittmannglenn@users.noreply.github.com>
2020-02-05 23:27:50 +0100

    Update alignment.py

--- pmlab_lite/alignments/alignment.py
+++ pmlab_lite/alignments/alignment.py
@@ -1,6 +1,4 @@
-#constant string
 #class to store te specifications if the found alignment(s)
-#merge this with a_star.py so that file == alignment.py and alignment is one class with astar as member function
 class Alignment:
 	def __init__(self):
 		self.alignment_move = []
