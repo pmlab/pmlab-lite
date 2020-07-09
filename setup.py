80718868567667daba607f240559c187a38001ab
Glenn <dittmang@hu-berlin.de>
2020-07-09 21:22:07 +0200

    rm setup.cfg, updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
 	name='pmlab_lite',
     version='0.3.9.8',
-    license='GPL-3.0',
-    description='Process Mining scripting environment',
     author=['Josep Carmona',
 			'Matthias Weidlich',
 			'Andrea Burattin',
@@ -12,7 +13,12 @@ setup(
 			'Simon Remy',
 			'Glenn Dittmann'],
     author_email='',
+    description='Process Mining scripting environment',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/pmlab/pmlab-lite',
+    packages=setuptools.find_packages(),
+    license='GPL-3.0',
 	install_requires=[
     		'numpy',
     		'graphviz',
