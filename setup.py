34b2aeed920fea9a859e68a53806e58605d74e72
Glenn <dittmang@hu-berlin.de>
2020-07-09 20:04:56 +0200

    minor changes in setup.py, added setup.cfg for README.md and enhanced README.md

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
-import setuptools, pathlib
+from distutils.core import setup
 
-HERE = pathlib.Path(__file__).parent
-
-README = (HERE / "README.md").read_text()
-
-setuptools.setup(
+setup(
 	name='pmlab_lite',
-    version='0.1.2',
+    version='0.3.9.8',
+    license='GPL-3.0',
     description='Process Mining scripting environment',
-    long_description=README,
-    long_description_content_type='text/markdown',
-	classifiers=[
-			'Development Status :: 2 - Pre-Alpha',
-			'Programming Language :: Python :: 3'
-		  ],
-    url='https://github.com/pmlab/pmlab-lite',
     author=['Josep Carmona',
 			'Matthias Weidlich',
 			'Andrea Burattin',
 			'jmunozgama',
-			'Simon Remy'],
+			'Simon Remy',
+			'Glenn Dittmann'],
     author_email='',
-    license='GPL-3.0',
-    packages=setuptools.find_packages(),
-    zip_safe=False
+    url='https://github.com/pmlab/pmlab-lite',
+	install_requires=[
+    		'numpy',
+    		'graphviz',
+    		'tqdm',
+    	], #maybe pythone-dateutil
+	classifiers=[
+			'Development Status :: 2 - Pre-Alpha',
+			'Programming Language :: Python :: 3',
+		  ],
 )
