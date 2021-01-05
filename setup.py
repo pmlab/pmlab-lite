ab350b8e0714109e462e0cba3dcfe1c72c9da0e6
Glenn <dittmang@hu-berlin.de>
2021-01-05 19:20:21 +0100

    colab badge

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name='pmlab_lite',
-    version='0.4.4.2',
+    version='0.4.4.3',
     author=['Josep Carmona',
 			'Matthias Weidlich',
 			'Andrea Burattin',
