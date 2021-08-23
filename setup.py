9aec2f557c3b0af077736947214150c596e6c857
Glenn <dittmang@hu-berlin.de>
2021-08-23 16:03:46 +0200

    update to 0.4.6.2 (upload to pypi)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pmlab_lite',
-    version='0.4.6.1',
+    version='0.4.6.2',
     author=['Josep Carmona',
             'Matthias Weidlich',
             'Andrea Burattin',
