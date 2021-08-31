ca08d31d1a905813af359d55a621f362f846b2fe
glennDittmann <glenn.dittmann@posteo.de>
2021-08-31 13:07:52 +0200

    update to PEP-8

--- pmlab_lite/helper/viz/__init__.py
+++ pmlab_lite/helper/viz/__init__.py
@@ -2,5 +2,5 @@
 This package contains classes and methods to visualize data structures.
 
 Files:
-	- dot.py: visualize Petri nets as DOT objects
+    - dot.py: visualize Petri nets as DOT objects
 """
