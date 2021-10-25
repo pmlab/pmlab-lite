5b40d26ed777232e6edd2b226875bc651050b03e
glennDittmann <glenn.dittmann@posteo.de>
2021-10-25 17:47:34 +0200

    change to new version of python packaging (using build)

--- distribute_to_pypi.sh
+++ distribute_to_pypi.sh
@@ -7,13 +7,13 @@ twinePath=$(which twine)                        # if still not working (output o
 
 
 # delete all directiories and files, that will be reuploaded / are created by sdist bdist_wheel command
-rm -r build dist pmlab_lite.egg-info
+rm -r dist pmlab_lite.egg-info
 
 # create package files
-python3 setup.py sdist bdist_wheel
+python3 -m build  # setup.py sdist bdist_wheel
 
 # check and upload to twine
 # exchange twine with $twinePath, if not working
-echo $twinePath
-twine check dist/*
-twine upload dist/*
+echo "$twinePath"
+twine check dist/*  # or instead of 'twine' use 'python3 -m twine', if you installed twine via pip
+twine upload dist/*  # same here
